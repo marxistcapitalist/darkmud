ee5e3010894080e05a8549f399d9abc07baf50ac
MarxistCapitalist <webmaster@syrukide.net>
2016-05-21 17:36:17 -0400

    Fix paths for exports
    Reduce work required to get ready for export (one line must be changed
    in Main to export JAR)

--- eclipseworkspace/skeleton/src/application/view/icons/LogResources.java
+++ eclipseworkspace/skeleton/src/application/view/icons/LogResources.java
@@ -1,14 +1,16 @@
 package application.view.icons;
 
+import application.Main;
+
 public abstract class LogResources {
 
-	public static final String maze = "view/icons/maze.png";
-	public static final String chat = "view/icons/chat.png";
-	public static final String room = "view/icons/room.png";
-	public static final String item = "view/icons/item.png";
-	public static final String kill = "view/icons/kill.png";
-	public static final String start = "view/icons/start.png";
-	public static final String end = "view/icons/end.png";
-	public static final String def = "view/icons/def.png";
-	public static final String response = "view/icons/response.png";
+	public static final String maze = Main.prepend + "icons/maze.png";
+	public static final String chat = Main.prepend + "icons/chat.png";
+	public static final String room = Main.prepend + "icons/room.png";
+	public static final String item = Main.prepend + "icons/item.png";
+	public static final String kill = Main.prepend + "icons/kill.png";
+	public static final String start = Main.prepend + "icons/start.png";
+	public static final String end = Main.prepend + "icons/end.png";
+	public static final String def = Main.prepend + "icons/def.png";
+	public static final String response = Main.prepend + "icons/response.png";
 }
---
+++ eclipseworkspace/skeleton/src/application/view/icons/LogResources2.java
@@ -0,0 +1,14 @@
+package application.view.icons;
+
+public abstract class LogResources2 {
+
+	public static final String maze = "view/icons/maze.png";
+	public static final String chat = "view/icons/chat.png";
+	public static final String room = "view/icons/room.png";
+	public static final String item = "view/icons/item.png";
+	public static final String kill = "view/icons/kill.png";
+	public static final String start = "view/icons/start.png";
+	public static final String end = "view/icons/end.png";
+	public static final String def = "view/icons/def.png";
+	public static final String response = "view/icons/response.png";
+}
